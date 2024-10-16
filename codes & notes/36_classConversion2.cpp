267e0722f6676c9a4c9cd34f3e7b75a7048fc03e
Keshav Chandel <chandelkeshav4@gmail.com>
2024-10-16 22:50:36 +0530

    initial commit

--- codes & notes/36_classConversion2.cpp
+++ codes & notes/36_classConversion2.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 // 2) using conversion operator
+class Farenheit{
+    int farenheit;
+    public:
+    Farenheit(int f):farenheit(f){}
+    void display(){
+        cout<<farenheit;
+    }
+};
+class Celcius{
+    int celcius;
+    public:
+    Celcius(int c):celcius(c){}
+    operator Farenheit(){
+        int f = (celcius*9/5)+32;
+        return Farenheit(f);
+    }
+};
+
 int main(){
-    
-
-
+    Celcius temp(44);
+    Farenheit temp2 = temp;
+    temp2.display();
 return 0;
 }
---
+++ codes & notes/37_AbstractClasses.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Shape{
+    public:
+    virtual int draw()=0;
+    void display(){
+        cout<<"This is shape"<<endl;
+    }
+};
+class circle:public Shape{
+    int radius;
+    public:
+    circle(int r):radius(r){}
+    int draw() override{
+        return 3.141*radius*radius;
+    }
+};
+class square:public Shape{
+    int lenght;
+    public:
+    square(int l):lenght(l){}
+    int draw() override {
+        return lenght*lenght;
+    }
+};
+int main(){
+    circle obj(2);
+    cout<<"The area of the circle is: "<<obj.draw()<<endl;
+    obj.display();
+
+    square obj1(2);
+    cout<<"The area of the square is: "<<obj1.draw()<<endl;
+    obj1.display();
+
+
+return 0;
+}
