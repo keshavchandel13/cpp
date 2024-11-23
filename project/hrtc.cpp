3737161ce60fb7904f412190fe3d2d561580c209
Keshav Chandel <chandelkeshav4@gmail.com>
2024-11-23 23:50:36 +0530

    added bus class

--- project/hrtc.cpp
+++ project/hrtc.cpp
@@ -2,11 +2,41 @@
 #include<vector>
 #include<string>
 using namespace std;
+
+//------------------------------------->> Bus class
+class Bus{
+    string route,driverName,timing;
+    int seats[42], busId;
+    string pass = "hp22";
+    Bus(string r, string d, string t, int s, int b){
+        route = r;
+        driverName = d;
+        timing = t;
+        fill(begin(seats), end(seats), 0); 
+        busId = b;
+    }
+    void addBus(){ // Add bus function (access for admin only)
+        string p;
+        cout<<"$$$$$ ADMIN PORTAL $$$$$";
+        cout<<"Enter the password"<<endl;
+        cin>>p;
+        if(pass == p){
+            cout<<"Welcome Sir!\n";
+            cout<<"Enter the route:\n"; cin>>route;
+            cout<<"Enter the driver name:\n"; cin>>driverName;
+            cout<<"Enter the bus timing:\n"; cin>>timing;
+            cout<<"Enter the bus Id:\n"; cin>>busId;
+        }
+        else{
+            cout<<"Wrong Password. Try again later"<<endl;
+        }
+    }
+};
 int main(){
     int choice;
     while(true){
         cout<<"******** Welcome to Himachal Road Transport Corporation ************";
-        cout << "\n1.Add Bus\n"
+        cout << "\n1. Add Bus\n"
         <<"2. View Buses\n"
         <<"3. Book Ticket\n"
         <<"4. Cancel Ticket\n"
