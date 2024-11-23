7dadb3ec396d062bb9fe69360367aca4ab66acca
Keshav Chandel <chandelkeshav4@gmail.com>
2024-11-23 22:58:11 +0530

    initial commit

---
+++ codes & notes/46_ClassTemplate.cpp
@@ -0,0 +1,31 @@
+// Class Template
+#include<iostream>
+using namespace std;
+template <class Type>
+class stack{
+    Type st[50];
+    int top;
+    public:
+    stack(){
+        top =-1;
+    }
+    void push(Type x){
+        st[++top]=x;
+        cout<<"Process completed\n";
+    }
+    Type pop(){
+        return st[top--];
+    }
+};
+int main(){
+    stack<int> s1;
+    s1.push(12);
+    s1.push(13);
+    s1.push(14);
+    s1.push(15);
+    cout<<s1.pop()<<endl;
+    cout<<s1.pop();
+
+
+    return 0;
+}
