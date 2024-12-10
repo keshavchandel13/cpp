5d8e3d2302d2c2f9a25485483ea63a2e9b58518e
Keshav Chandel <chandelkeshav4@gmail.com>
2024-12-10 23:07:39 +0530

    initial commit

--- codes & notes/50_ExceptionHandling.cpp
+++ codes & notes/50_ExceptionHandling.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
+class stack{
+    int st[50];
+    int top;
+    public:
+    stack(){
+        top=-1;
+    };
+    void push(int x){
 
+        st[++top]=x;
+    }
+    int  pop(){
+        return st[top--];
+    };
+    void display(){
+        for(int i =0 ;i<=top;i++){
+            cout<<st[i]<<endl;
+        }
+    }
+};
 int main(){
+    stack obj;
+    obj.push(10);
+    obj.push(20);
+    obj.push(30);
+    obj.display();
 
 
 return 0;
