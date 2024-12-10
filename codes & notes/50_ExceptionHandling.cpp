994bc5aafcc09ef5990e998dc633bd8bad1ded31
Keshav Chandel <chandelkeshav4@gmail.com>
2024-12-10 23:38:26 +0530

    initial commit

--- codes & notes/50_ExceptionHandling.cpp
+++ codes & notes/50_ExceptionHandling.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 class stack{
-    int st[50];
+    int st[5];
     int top;
     public:
     stack(){
         top=-1;
     };
+    class full{};
+    class empty{};
     void push(int x){
+        if(top>=4){
+            throw full();
+        }
 
         st[++top]=x;
     }
     int  pop(){
+        if (top==-1){throw empty();}
         return st[top--];
     };
     void display(){
@@ -22,10 +28,22 @@ class stack{
 };
 int main(){
     stack obj;
-    obj.push(10);
-    obj.push(20);
-    obj.push(30);
-    obj.display();
+    try{
+        // obj.pop();
+        obj.push(10);
+        obj.push(20);
+        obj.push(30);
+        obj.push(40);
+        obj.push(50);
+        obj.push(60);
+        obj.display();
+    }
+    catch (stack::full){
+        cout<<"The stack is full\n";
+    }
+    catch (stack::empty){
+        cout<<"The stack is empty\n";
+    }
 
 
 return 0;
